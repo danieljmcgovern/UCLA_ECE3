55b84c37852a6af4a7ef639070cbb277d4553a62
danieljmcgovern <58084356+danieljmcgovern@users.noreply.github.com>
2020-11-17 09:19:54 -0800

    added execution of donut at end of strip

--- ECE3_project/ECE3_project.ino
+++ ECE3_project/ECE3_project.ino
@@ -1,8 +1,6 @@
 
 #include <ECE3.h>
 
-//TODO add reset switch
-
 const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
@@ -18,7 +16,7 @@ uint16_t SVraw[8];
 float weighted_sum = 0;
 float weight_8421[8] = {-8.0, -4.0, -2.0, -1.0, 1.0, 2.0, 4.0, 8.0}; 
 
-//these values are from trial3 fusion 11-14-20                                      // TODO add sensor calibration... using bumper swithc
+//these values are from trial3 fusion 11-14-20                                      
 float SVmin[8] = {563.2, 586.0, 654.2, 496.0, 585.2, 586.0, 585.8, 677.2};            //min values from the sensor calibration test, {SV1, SV2, ... SV8}
 float SVmax[8] = {1936.8, 1914.0,  1843.8, 1374.4, 1491.2, 1886.6, 1606.6, 1822.8};
 float SVnorm[8];  //SVnorm = (SVraw - SVmin)*(1000/SVmax)
@@ -49,15 +47,14 @@ void setup()
   pinMode(PUSH1, INPUT_PULLUP);
   attachInterrupt(PUSH1, calibrate_min, FALLING);
   pinMode(PUSH2, INPUT_PULLUP);
-  attachInterrupt(PUSH2, calibrate_max, FALLING); 
-  
+  attachInterrupt(PUSH2, calibrate_max, FALLING);  
 
   delay(2000);
 }
 
-int leftSpd = 20;
-int rightSpd = 20;
-int constSpd = 20;  //using this because leftSpd will no longer be a constant reference point.
+int leftSpd = 0;
+int rightSpd = 0;
+int constSpd = 20;  //starting motor speed that PD controller will modify
 
 float error = 0;
 float error_prev = 0;
@@ -66,12 +63,14 @@ float derivative = 0;
 float kp = 0.10;    //constant of proportionality
 float kd = 0.10;    //constant of differentiation
 
+int count = 0;
+int raw_sum = 0;
+
 void loop()
 {  
   weighted_sum = 0; //this value must be zeroed out becuase it is only ever added to in the for loop, ie it's value is never reset (as is error's value)
-  
-  // read raw sensor values
-  ECE3_read_IR(SVraw);
+   
+  ECE3_read_IR(SVraw);   // read raw sensor values
 
   for (int i = 0; i<8; i++){    //normalize the raw sensor data
     SVnorm[i] = (SVraw[i] - SVmin[i])*(1000.0/SVmax[i]);
@@ -84,11 +83,9 @@ void loop()
   derivative = error - error_prev;
 
   leftSpd = constSpd - kp*error - derivative*kd;
-  rightSpd = constSpd + kp*error + derivative*kd;
+  rightSpd = constSpd + kp*error + derivative*kd;  
 
-  
-
-  //printing the output of the PD controller to the serial monitor to validate results. once validated will send speed changes to motors instead of printing them.
+  //printing the output of the PD controller to the serial monitor to validate results.
   /*
   Serial.print("error: ");   Serial.println(error); 
   Serial.print("E*kp: ");   Serial.println(error*kp);
@@ -98,15 +95,42 @@ void loop()
   Serial.println();*/
  
   error_prev = error;
-  //delay(50);    //TODO: how long of a delay is necessary? is it necessary? how rapidly do sensor values come in?
-  //got rid of this delay and the car went from only following straight path and failing on curved, to being able to follow curved path pretty well.
   
- // analogWrite(left_pwm_pin,leftSpd);
- // analogWrite(right_pwm_pin,rightSpd);
+  analogWrite(left_pwm_pin,leftSpd);
+  analogWrite(right_pwm_pin,rightSpd);
+
+  //code to execute 180 on reaching 1st black strip
+  raw_sum = 0;
+  for(int i = 0; i < 8; i++){
+    raw_sum += SVraw[i];
+  }
+  if((raw_sum / 8)> 2000 && count ==0){//if the pins mostly detect black, begin to donut
+    analogWrite(left_pwm_pin,0);       //pause before executing donut
+    analogWrite(right_pwm_pin,0);
+    delay(200);
+    digitalWrite(right_dir_pin, HIGH);  //reverse direction of right motor
+    analogWrite(left_pwm_pin,30);
+    analogWrite(right_pwm_pin,30);
+    delay(2000);                        //execute turn for duration of delay
+    analogWrite(left_pwm_pin,0);       //pause after 180 is executed
+    analogWrite(right_pwm_pin,0);
+    delay(100);
+    digitalWrite(right_dir_pin, LOW);     //reset direction of right motor to go forward
+    analogWrite(left_pwm_pin,30);       //go forward to get away from the black strip
+    analogWrite(right_pwm_pin,30);
+    delay(200);
+    count++;
+  }
+ //code to execute stop on reaching 2nd black strip
+  /*if((raw_sum / 8)> 2000 && count ==1){   //if seeing the black strip a second time... car is at end of track
+    analogWrite(left_pwm_pin,0);       //stop the car
+    analogWrite(right_pwm_pin,0);
+    delay(10000);
+  }*/
+    
 }
 
-//11/16 7:30pm got the push buttons working. wasted a bunch of time trying to use bumper switches to do this.
-//11/17 7:30am added sensor calibrations. calibrate_min can only run once, not sure why. cal_max can run multiple times no prob.
+//TODO FIX: calibrate_min can only run once, not sure why. calibrate_max can run multiple times
 void calibrate_min() {
   ECE3_read_IR(SVraw);              //read raw sensor data
   for (int i = 0; i<8; i++){       //zero out SVmin array
@@ -130,6 +154,9 @@ void calibrate_max() {
   for (int i = 0; i<10; i++){     //taking 10 readings per sensor
     for (int i = 0; i<8; i++)     //take a reading from each sensor
       SVmax[i] += (SVraw[i]/10.0);//ten values going in, so divide each value by 10 for average
+  }
+  for (int i = 0; i<8; i++){
+    SVmax[i] -= SVmin[i];         //subtract min values
   }
   for (int i = 0; i<8; i++){
     Serial.print(SVmax[i]); Serial.print("\t");

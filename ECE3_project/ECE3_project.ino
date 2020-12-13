907ac4c27ef9350d4185c0117185728a45b9bef6
danieljmcgovern <58084356+danieljmcgovern@users.noreply.github.com>
2020-12-12 20:05:45 -0800

    cleaned up and commented code for inclusion in final report.

--- ECE3_project/ECE3_project.ino
+++ ECE3_project/ECE3_project.ino
@@ -9,31 +9,18 @@ const int right_nslp_pin=11; // nslp ==> awake & ready for PWM
 const int right_dir_pin=30;
 const int right_pwm_pin=39;
 
-const int LED_RF = 41;
+const int LED_RF = 41;    //debugging led
 
-uint16_t SVraw[8];
+uint16_t SVraw[8];    //array to hold raw sensor data
 
 float weighted_sum = 0;
 float weight_8421[8] = {-8.0, -4.0, -2.0, -1.0, 1.0, 2.0, 4.0, 8.0}; 
 
-//these values are from trial3 fusion 11-14-20                                      
-//float SVmin[8] = {563.2, 586.0, 654.2, 496.0, 585.2, 586.0, 585.8, 677.2};            //min values from the sensor calibration test, {SV1, SV2, ... SV8}
-//float SVmax[8] = {1936.8, 1914.0,  1843.8, 1374.4, 1491.2, 1886.6, 1606.6, 1822.8};
-
-//these values are from "sensor fusion with sharpie strip"                                     
-//float SVmin[8] = {513.2,  562.6, 630.2, 450.6, 539.8, 553.6, 562.6, 653.4};        
-//float SVmax[8] = {1444,  1069.8,  1422.4,  948, 1185.6,  1101.8,  1185.4,  1846.6};
-
-//these values are from on the fly calibration using button code   , 11/29                                  
-float SVmax[8] = {1977,  2205,  2500,  1841,  1728,  2275,  2114,  2500  };    
-float SVmin[8] = {653,  698, 768, 630, 698, 675, 698, 792 };
-
+//min values from the sensor calibration test, {SV1, SV2, ... SV8}                              
+float SVmin[8] = {563.2, 586.0, 654.2, 496.0, 585.2, 586.0, 585.8, 677.2};            
+float SVmax[8] = {1936.8, 1914.0,  1843.8, 1374.4, 1491.2, 1886.6, 1606.6, 1822.8};
 
 float SVnorm[8];  //SVnorm = (SVraw - SVmin)*(1000/SVmax)
-
-
-
-int pause = 0;
 
 void setup()
 {
@@ -54,9 +41,9 @@ void setup()
   digitalWrite(right_dir_pin,LOW);
   digitalWrite(right_nslp_pin,HIGH);
 
-  pinMode(LED_RF, OUTPUT); 
+  pinMode(LED_RF, OUTPUT);    //LED used for debuggin in sensor calibration
 
-  pinMode(PUSH1, INPUT_PULLUP);
+  pinMode(PUSH1, INPUT_PULLUP);     //pins for use in on the fly sensor calibration
   attachInterrupt(PUSH1, calibrate_min, FALLING);
   pinMode(PUSH2, INPUT_PULLUP);
   attachInterrupt(PUSH2, calibrate_max, FALLING);  
@@ -64,19 +51,19 @@ void setup()
   delay(2000);
 }
 
-int leftSpd = 0;
+int leftSpd = 0;    //first iteration of PD control adjusts these values, ok to leave zeroed here.
 int rightSpd = 0;
-int constSpd = 50;  //starting motor speed that PD controller will modify
+int constSpd = 40;  //starting motor speed that PD controller will modify
 
-float error = 0;
-float error_prev = 0;
+float error = 0;      //sensor fusion error value
+float error_prev = 0; //previous error
 float derivative = 0;
 
-float kp = 0.20 ;    //constant of proportionality
+float kp = 0.10 ;    //constant of proportionality
 float kd = 0.10;    //constant of differentiation  
 
-int count = 0;
-int raw_sum = 0;  
+int count = 0;      //counter used in detecting black strip
+int raw_sum = 0;    //all eight sensor values will be summed, then used to detect the black strip
 
 void loop()
 {  
@@ -84,52 +71,47 @@ void loop()
    
   ECE3_read_IR(SVraw);   // read raw sensor values
 
+  raw_sum = 0;                  //zero out raw_sum to erase values from previous loop
   for (int i = 0; i<8; i++){    //normalize the raw sensor data
     SVnorm[i] = (SVraw[i] - SVmin[i])*(1000.0/SVmax[i]);
-  }
-  for (int i = 0; i<8; i++){    //compute weighted sum on normalized value
     weighted_sum += SVnorm[i]*weight_8421[i];
+    raw_sum += SVraw[i];
   }
-  error = weighted_sum/4.0;
+  error = weighted_sum/4.0;           //be sure to adjust the value weighted sum is divided by if the weighting scheme is changed
 
-  derivative = error - error_prev;
-
+  derivative = error - error_prev;    //defining the derivative term
+  
+  //the PD control statements:
   leftSpd = constSpd - kp*error - derivative*kd;
   rightSpd = constSpd + kp*error + derivative*kd;  
  
-  error_prev = error;
+  error_prev = error;                     //current error for this loop becomes previous error for next loop
   
-  analogWrite(left_pwm_pin,leftSpd);
+  analogWrite(left_pwm_pin,leftSpd);        //sets the motor speed after the PD control has adjusted the speed
   analogWrite(right_pwm_pin,rightSpd);
 
   //code to execute 180 on reaching 1st black strip
-  raw_sum = 0;  
-  for(int i = 0; i < 8; i++){
-    raw_sum += SVraw[i];
-  }
-   if((raw_sum / 8)> 1800 && (count == 0)){
+   if((raw_sum / 8)> 2000 && (count == 0)){ //if all the senors are reading above 2000, "all black"
     digitalWrite(right_dir_pin, HIGH);      //reverse direction of right motor
     analogWrite(left_pwm_pin,120);
     analogWrite(right_pwm_pin,120);          //execute turn for duration of delay
     delay(400);
-    digitalWrite(right_dir_pin, LOW);
-    raw_sum = 0;                              //reset raw_sum so next if statment doesn't immediately execute
-    count++;
+    digitalWrite(right_dir_pin, LOW);       //reset motor direction to go forward
+    raw_sum = 0;                            //reset raw_sum so next if statment doesn't immediately execute
+    count++;                                //counter to keep this if stmt from executing multiple times
   }
   //code to detect the 2nd strip, end of track
   if((raw_sum / 8)> 2000 && (count == 1)){
     analogWrite(left_pwm_pin, 0);           //stop the car
     analogWrite(right_pwm_pin, 0);
     delay(10000);
-  }
-  
-    
+  }      
 }
 
-//TODO FIX: calibrate_min can only run once, not sure why. calibrate_max can run multiple times
+//function for obtaining fresh sensor calibration data. access via top buttons on car
 void calibrate_min() {
   analogWrite(left_pwm_pin, 0);           //stop the car
-    analogWrite(right_pwm_pin, 0);
+  analogWrite(right_pwm_pin, 0);
   ECE3_read_IR(SVraw);              //read raw sensor data
   for (int i = 0; i<8; i++){       //zero out SVmin array
     SVmin[i] = 0;
@@ -146,7 +128,7 @@ void calibrate_min() {
 }
 void calibrate_max() {
   analogWrite(left_pwm_pin, 0);           //stop the car
-    analogWrite(right_pwm_pin, 0);
+  analogWrite(right_pwm_pin, 0);
   ECE3_read_IR(SVraw);              //read raw sensor data
   for (int i = 0; i<8; i++){       //zero out SVmax array
     SVmax[i] = 0;
@@ -162,7 +144,5 @@ void calibrate_max() {
     Serial.print(SVmax[i]); Serial.print("\t");
   }
   Serial.println();
-  leftSpd = 0;
-  rightSpd = 0;
   delay(5000);
 }
